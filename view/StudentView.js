647c6b35a36d587c9fd4cc0349e36352e96dd631
Felix Gustavo <gustavovieira1945@gmail.com>
2021-03-18 11:00:16 -0300

    refactor table: change formatting fields

--- view/StudentView.js
+++ view/StudentView.js
@@ -6,7 +6,7 @@ class StudentView {
 	_renderView = (students) => {
 		this._students = students;
 		return `
-		<table class="table table-hover table-bordered">
+		<table class="table table-hover">
 			<thead>
 				<tr>
 					<th>Nome</th>
@@ -21,43 +21,43 @@ class StudentView {
 		
 			<tbody>
 				${students.map(student => `
-						<tr>
-							<td>${student.name}</td>
-							<td>${student.firstGrade}</td>
-							<td>${student.secondGrade}</td> 
-							<td>${student.frequency}</td>
-							<td>${student.finalTest}</td>
-							<td>${student.avg}</td>
-							<td>${student.status ? 'Aprovado' : 'Reprovado'}</td>
-						</tr>          
-					`).join('')}
+					<tr>
+						<td>${student.name}</td>
+						<td>${student.firstGrade}</td>
+						<td>${student.secondGrade}</td> 
+						<td>${student.frequency}</td>
+						<td>${student.finalTest == '' ? '-' : student.finalTest}</td>
+						<td>${student.avg}</td>
+						<td>${student.status ? 'Aprovado' : 'Reprovado'}</td>
+					</tr>          
+				`).join('')}
 			</tbody>
 
 			<tfoot>
 				<tr>
-						<th>Aprovados</th>
-						<td>
-							${
-								students.filter(student => student.status)
-												.reduce(total => total + 1, 0)
-							}
-						</td>
+					<th>Aprovados</th>
+					<td>
+						${
+							students.filter(student => student.status)
+											.reduce(total => total + 1, 0)
+						}
+					</td>
 
-						<th>Reprovados</th>
-						<td>
-							${
-								students.filter(student => !student.status)
-												.reduce(total => total + 1, 0)
-							}
-						</td>
+					<th>Reprovados</th>
+					<td>
+						${
+							students.filter(student => !student.status)
+											.reduce(total => total + 1, 0)
+						}
+					</td>
 
-						<th>Média da Turma</th>
-						<td>
-							${
-								(students.reduce((total, student) => 
-									total + student.avg, 0)) / students.length
-								}
-						</td>
+					<th>Média da Turma</th>
+					<td>
+						${
+							(students.reduce((total, student) => 
+								total + student.avg, 0)) / students.length | 0
+						}
+					</td>
 				</tr>
 			</tfoot>
 		</table>
